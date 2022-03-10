709302b86dcf731270fb15016ddf181a116af4e8
syabdull <syab5050@gmail.com>
2022-03-10 10:34:07 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,10 @@
+console.log("hello there");
+console.log("iam Abdulla");
+let num=10;
+console.log(num);
+let char='a';//character
+console.log(num);
+console.log(char);
+let myString="Abdulla";//String
+ let myBoolean=true;//boolean
+ console.log(myBoolean);
