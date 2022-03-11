0ec071a4f999b906f76b2fd3931b7e94065ae1c7
syabdull <syab5050@gmail.com>
2022-03-11 12:21:30 +0530

    nexted object

--- obj.js
+++ obj.js
@@ -5,3 +5,15 @@ let obj1={"name":"Abdulla",
               Address:12,
               lastName:'Aman'};
               console.log(obj1);
+              // 
+              let capAmerica={
+
+                Name:"steve",
+                Age:999,
+                friends:['natasha','thor','tony','bucky','bruce'],
+                address:{
+                    city:"queens",
+                    state:'haryana',
+                }
+              }
+              console.log(capAmerica);
