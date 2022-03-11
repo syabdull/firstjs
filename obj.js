31a82586f772e89d6b9d6c0bcd70a5acca225e47
syabdull <syab5050@gmail.com>
2022-03-11 11:50:53 +0530

    first obj creating

---
+++ obj.js
@@ -0,0 +1,7 @@
+// creating  obj
+let obj1={"name":"Abdulla",
+            age:45,
+             car:true,
+              Address:12,
+              lastName:'Aman'};
+              console.log(obj1);
--- string.js
+++ string.js
@@ -4,3 +4,15 @@ console.log(ab);
 console.log(ab.length);
 //Methods of Strings
 console.log(ab.slice(3,6));
+//yet  another example for slice method 
+let schoole="oldSchool";
+let newSchoole=schoole.slice(2,5);
+console.log(newSchoole);
+// replace method 
+let bot="popped is cool";
+  let bor=bot.replace("p","b");
+  console.log(bor);
+  console.log(bot);
+  //to uppercase Method
+  //to lowercase Method
+  d
